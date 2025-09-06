18b25fa6a90d4bc823f77742dc986526f5649bf4
10974-spec <emmanuelnyakoe13@gmail.com>
2025-09-06 21:35:52 +0300

    sidebar

--- client/src/components/admin/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
 import { LayoutDashboardIcon, ListCollapseIcon, ListIcon, PlusSquareIcon } from 'lucide-react'
+import { NavLink } from 'react-router-dom'
 
 const AdminSidebar = () => {
 
@@ -11,17 +12,32 @@ const AdminSidebar = () => {
     }
 
     const  adminNavLinks = [
-        { name: 'Dashboard', href: '/admin', icon:LayoutDashboardIcon },
-        { name: 'Add Shows', href: '/admin/add-shows',icon: PlusSquareIcon },
-        { name: 'List Shows', href: '/admin/list-shows', icon: ListIcon},
-        { name: 'List Bookings', href: '/admin/list-bookings', icon: ListCollapseIcon },
+        { name: 'Dashboard', path: '/admin', icon:LayoutDashboardIcon },
+        { name: 'Add Shows', path: '/admin/add-shows',icon: PlusSquareIcon },
+        { name: 'List Shows', path: '/admin/list-shows', icon: ListIcon},
+        { name: 'List Bookings', path: '/admin/list-bookings', icon: ListCollapseIcon },
     ]
 
 
   return (
-    <div>
-        <h1>sidebar</h1>
-
+    <div className='h-[calc(100vh-64px)] md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-gray-300/20 text-sm'>
+        <img src={user.imageUrl} alt="sidebar" className='h-9 md:h-14 w-9 md:w-14 rounded-full mx-auto' />
+        <p className="mt-2 text-base  max-md:hidden">
+            {user.firstName} {user.lastName}
+        </p>
+        <div className="w-full">
+            {adminNavLinks.map((link, index)=>(
+                <NavLink key={index} end to={link.path} className={({isActive}) => `relative flex items-center max-md:justify-center gap-2 w-full py-2.5 min-md:pl-10 first:mt-6 text-gray-400 ${isActive && 'bg-primary/25 text-primary group'}`}>
+                    {({isActive})=>(
+                        <>
+                        <link.icon className="w-5 h-5"/>
+                        <p className="max-md:hidden">{link.name}</p>
+                        <span className={`w-1.5 h-10 rounded-1 right-0 absolute ${isActive && 'bg-primary'}`}/>
+                        </>
+                    )}
+                </NavLink>
+            ))}
+        </div>
     </div>
   )
 }
